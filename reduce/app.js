0f4df6f4d80dbd8165917eb8ed936b69bb7dae76
DESKTOP-MTDGO5G\AMIR <amiralianwar611@gmail.com>
2022-03-15 19:07:47 +0500

    inital commit for reduce setup

---
+++ reduce/app.js
@@ -0,0 +1,9 @@
+// filter method iterates over the array
+//returns single value and whatever you want to return from reduce like number, object ,array
+const myfriends = [
+    { name: 'Mubahsar', age: 26, position: 'developer', salary: 100 },
+    { name: 'SQA', age: 25, position: 'SQA', salary: 300 },
+    { name: 'Haroon', age: 24, position: 'SQA', salary: 400 },
+    { name: 'Mamoon', age: 25, position: 'Senior Full stack', salary: 1000 },
+    { name: 'Amir', age: 25, position: 'Full stack', salary: 1000 },
+];
