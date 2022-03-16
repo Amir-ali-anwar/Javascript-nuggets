72e551fc83ab4c3a901b0b09ff5e8e1b3d114e19
Amir-ali-anwar <67230630+Amir-ali-anwar@users.noreply.github.com>
2022-03-16 15:59:43 +0500

    Wip:Reduce implementation

--- reduce/app.js
+++ reduce/app.js
@@ -1,9 +1,17 @@
 // filter method iterates over the array
 //returns single value and whatever you want to return from reduce like number, object ,array
 const myfriends = [
-    { name: 'Mubahsar', age: 26, position: 'developer', salary: 100 },
-    { name: 'SQA', age: 25, position: 'SQA', salary: 300 },
-    { name: 'Haroon', age: 24, position: 'SQA', salary: 400 },
     { name: 'Mamoon', age: 25, position: 'Senior Full stack', salary: 1000 },
-    { name: 'Amir', age: 25, position: 'Full stack', salary: 1000 },
+    { name: 'Mubahsar', age: 26, position: 'developer', salary: 100 },
+    { name: 'Babar', age: 25, position: 'SQA', salary: 3000 },
+    { name: 'Haroon', age: 24, position: 'SQA', salary: 400 },
+    { name: 'Amir', age: 25, position: 'Full stack', salary: 50 },
 ];
+const dailyTotal = myfriends.reduce((total, person) => {
+
+    console.log(total)
+    console.log(person)
+    total += person.salary
+    return total
+}, 0);
+console.log(dailyTotal)
