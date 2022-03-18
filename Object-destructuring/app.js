b0a8e00c59abff4de0ee5687644fcce9837ff131
Amir-ali-anwar <67230630+Amir-ali-anwar@users.noreply.github.com>
2022-03-18 16:27:09 +0500

    Initial commit for Object destructuring

---
+++ Object-destructuring/app.js
@@ -0,0 +1,24 @@
+const employees = {
+    engineers: {
+        0: {
+            id: 1,
+            name: "Amir Ali",
+            occupation: "Fullstack Engineer"
+        },
+        1: {
+            id: 2,
+            name: "Mubashar Akram",
+            occupation: "Front-end Engineer"
+        },
+        2: {
+            id: 3,
+            name: "Babar mumtaz",
+            occupation: "SQA Engineer"
+        },
+        3: {
+            id: 4,
+            name: "Haroon hassan",
+            occupation: "SQA Engineer"
+        },
+    }
+};
