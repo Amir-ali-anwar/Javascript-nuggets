4dd568a644dce4443ccb505a491c3883a6de4c2e
Amir-ali-anwar <67230630+Amir-ali-anwar@users.noreply.github.com>
2022-03-21 17:10:00 +0500

    initial setup for promise

---
+++ Promise/app.js
@@ -0,0 +1,3 @@
+//Promise are used to run Asynchronous operations in javascript,
+//Prior to promises events and callback functions were used but they had limited functionalities and created unmanageable code.
+//Promise are use to avoid the callback hell
