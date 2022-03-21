20c18d56ca1ba3a848273ec0593f38ab4c13ec5f
Amir-ali-anwar <67230630+Amir-ali-anwar@users.noreply.github.com>
2022-03-21 17:18:50 +0500

    Completed the first example of js promise

--- Promise/app.js
+++ Promise/app.js
@@ -1,3 +1,21 @@
 //Promise are used to run Asynchronous operations in javascript,
 //Prior to promises events and callback functions were used but they had limited functionalities and created unmanageable code.
 //Promise are use to avoid the callback hell
+
+
+let Promises = new Promise((resolve, reject) => {
+    const firstvalue = 'uncle tom'
+    const secondvalue = 'uncle tom'
+    if (firstvalue === secondvalue) {
+        resolve()
+    } else {
+        reject()
+    }
+
+})
+
+Promises.then(() => {
+    console.log('Success');
+}).catch(() => {
+    console.log('Some error has occurred');
+});
