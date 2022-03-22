70679b1d318919387772f140e0b4b8ef585f8238
Amir-ali-anwar <67230630+Amir-ali-anwar@users.noreply.github.com>
2022-03-22 16:49:24 +0500

    another example of Js promise

--- Promise/app.js
+++ Promise/app.js
@@ -19,3 +19,31 @@ Promises.then(() => {
 }).catch(() => {
     console.log('Some error has occurred');
 });
+
+// Examples
+let success = true;
+
+function getUsers() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (success) {
+                resolve([
+                    { username: 'john', email: 'john@test.com' },
+                    { username: 'jane', email: 'jane@test.com' },
+                ]);
+            } else {
+                reject('Failed to the user list');
+            }
+        }, 1000);
+    });
+}
+
+function onFulfilled(users) {
+    console.log(users);
+}
+function onRejected(error) {
+    console.log(error);
+}
+
+const promise = getUsers();
+promise.then(onFulfilled, onRejected);
