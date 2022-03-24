84adbfbd874c2cadf5efaca6ca0f3fd8f7703881
DESKTOP-MTDGO5G\AMIR <amiralianwar611@gmail.com>
2022-03-24 20:55:46 +0500

    wip:working on js set

--- Unique-Values/app.js
+++ Unique-Values/app.js
@@ -1,2 +1,7 @@
 // new set is used to get the unique values from array
-//
+// Returns an object
+
+const data=[1,2,3,4,5,6,6,6,6,7,8,9,9,9,9,9,]
+let output= [... new Set(data)]
+console.log(output)
+console.log(typeof output)
