e7e040a5a20355a6865b1169638c12f8f3ef7e7b
DESKTOP-MTDGO5G\AMIR <amiralianwar611@gmail.com>
2022-03-24 21:03:47 +0500

    wip:working on js set

--- Unique-Values/app.js
+++ Unique-Values/app.js
@@ -3,5 +3,6 @@
 
 const data=[1,2,3,4,5,6,6,6,6,7,8,9,9,9,9,9,]
 let output= [... new Set(data)]
-console.log(output)
-console.log(typeof output)
+let objectToArray= Array.from(output)
+console.log(Array.isArray(objectToArray))
+console.log(typeof objectToArray)
