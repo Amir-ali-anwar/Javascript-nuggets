c9efe85a40208e71ea9bd17e724f2ed63f19fb46
DESKTOP-MTDGO5G\AMIR <amiralianwar611@gmail.com>
2022-03-29 19:25:07 +0500

    working on includes example

--- Includes/app.js
+++ Includes/app.js
@@ -1,2 +1,5 @@
 //iterates over an array and return true if given item is available 
 const foodItems= ['milk','water','sugar','jam'];
+
+const isIncluded= foodItems.includes('milk')
+console.log(isIncluded)
