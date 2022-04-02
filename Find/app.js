487a6281f12c7a4909f1dbdb182d4215a3b5291e
DESKTOP-MTDGO5G\AMIR <amiralianwar611@gmail.com>
2022-04-02 19:54:49 +0500

    completed one example for js find

--- Find/app.js
+++ Find/app.js
@@ -1,1 +1,15 @@
 //  returns a single value, always returns a single ,if no match found return undefined
+
+const myfriends = [
+    { name: 'Mamoon', age: 25, position: 'Senior Full stack', salary: 1000 },
+    { name: 'Mubahsar', age: 26, position: 'developer', salary: 100 },
+    { name: 'Babar', age: 25, position: 'SQA', salary: 6000 },
+    { name: 'Haroon', age: 24, position: 'SQA', salary: 400 },
+    { name: 'Amir', age: 25, position: 'Full stack', salary: 50 },
+];
+
+let isAvailable= myfriends.find((friend)=>friend.name==='Mamoon')
+console.log(isAvailable)
+
+ isAvailable= myfriends.find((friend)=>friend.name==='unce tom')
+console.log(isAvailable)
