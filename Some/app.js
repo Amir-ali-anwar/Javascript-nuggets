994a2fc8688931e887b72c2e9320c4f42c7a2d41
DESKTOP-MTDGO5G\AMIR <amiralianwar611@gmail.com>
2022-04-02 20:46:24 +0500

    worked on js some example

--- Some/app.js
+++ Some/app.js
@@ -1,5 +1,7 @@
-// filter method iterates over the array
-//returns single value and whatever you want to return from reduce like number, object ,array
+// filter method iterates over array
+//does not change the origional array
+//returns false if not element found
+//test of every element in an array
 const myfriends = [
     { name: 'Mamoon', age: 25, position: 'Senior Full stack', salary: 1000 },
     { name: 'Mubahsar', age: 26, position: 'developer', salary: 100 },
@@ -7,3 +9,12 @@ const myfriends = [
     { name: 'Haroon', age: 24, position: 'SQA', salary: 400 },
     { name: 'Amir', age: 25, position: 'Full stack', salary: 50 },
 ];
+ let isTrue= myfriends.some((friends)=>{
+     return friends.age>20
+ })   
+ console.log(isTrue)
+
+  isTrue= myfriends.some((friends)=>{
+    return friends.age===20
+})   
+console.log(isTrue)
