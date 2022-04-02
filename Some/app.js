7dfb911799da61172a366666c77e60b7ef47076c
DESKTOP-MTDGO5G\AMIR <amiralianwar611@gmail.com>
2022-04-02 19:57:59 +0500

    Fixing some typo mistakes

---
+++ Some/app.js
@@ -0,0 +1,9 @@
+// filter method iterates over the array
+//returns single value and whatever you want to return from reduce like number, object ,array
+const myfriends = [
+    { name: 'Mamoon', age: 25, position: 'Senior Full stack', salary: 1000 },
+    { name: 'Mubahsar', age: 26, position: 'developer', salary: 100 },
+    { name: 'Babar', age: 25, position: 'SQA', salary: 6000 },
+    { name: 'Haroon', age: 24, position: 'SQA', salary: 400 },
+    { name: 'Amir', age: 25, position: 'Full stack', salary: 50 },
+];
