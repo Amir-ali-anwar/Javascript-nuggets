152d57df602222609d273085c97ff806984f8e88
Amir-ali-anwar <67230630+Amir-ali-anwar@users.noreply.github.com>
2022-04-04 14:02:28 +0500

    Working on the setup of every operaton

---
+++ Every/app.js
@@ -0,0 +1,7 @@
+const myfriends = [
+    { name: 'Mamoon', age: 25, position: 'Senior Full stack', salary: 1000 },
+    { name: 'Mubahsar', age: 26, position: 'developer', salary: 100 },
+    { name: 'Babar', age: 25, position: 'SQA', salary: 6000 },
+    { name: 'Haroon', age: 24, position: 'SQA', salary: 400 },
+    { name: 'Amir', age: 25, position: 'Full stack', salary: 50 },
+];
