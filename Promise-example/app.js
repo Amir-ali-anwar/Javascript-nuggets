556a376eda3edaf54386c6cd8ab9778579e840b3
Amir-ali-anwar <67230630+Amir-ali-anwar@users.noreply.github.com>
2022-04-18 16:59:29 +0500

    promise example completed

--- Promise-example/app.js
+++ Promise-example/app.js
@@ -4,13 +4,24 @@ console.log(imgContainer)
 
 const url = 'https://source.unsplash.com/random';
 
-button.addEventListener('click', () => {
-    Loadimage()
+button.addEventListener('click', async () => {
+    try {
+        const resp = await Loadimage(url);
+        imgContainer.appendChild(resp)
+    } catch (error) {
+        console.log(error)
+    }
 })
 
-const Loadimage = () => {
+const Loadimage = (url) => {
     return new Promise((resolve, reject) => {
         let img = new Image();
-        console.log(img)
+        img.addEventListener('load', () => {
+            resolve(img)
+        })
+        img.addEventListener('error', () => {
+            reject(`there was error in you ${url}`)
+        })
+        img.src = url;
     })
 }
