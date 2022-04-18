1be160c746f209d4cd0a3120191117436934116a
Amir-ali-anwar <67230630+Amir-ali-anwar@users.noreply.github.com>
2022-04-18 16:41:59 +0500

    inital setup for promise example

--- Promise-example/app.js
+++ Promise-example/app.js
@@ -1,1 +1,16 @@
-let imgContainer = document
+const button = document.querySelector('.btn');
+const imgContainer = document.querySelector('.image-container');
+console.log(imgContainer)
+
+const url = 'https://source.unsplash.com/random';
+
+button.addEventListener('click', () => {
+    Loadimage()
+})
+
+const Loadimage = () => {
+    return new Promise((resolve, reject) => {
+        let img = new Image();
+        console.log(img)
+    })
+}
