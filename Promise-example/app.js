30d308c52837d98cf9b7e91da7593e99af4b155f
Amir-ali-anwar <67230630+Amir-ali-anwar@users.noreply.github.com>
2022-04-29 09:30:56 +0500

    added loading functinality

--- Promise-example/app.js
+++ Promise-example/app.js
@@ -1,11 +1,14 @@
 const button = document.querySelector('.btn');
 const imgContainer = document.querySelector('.image-container');
+const loadingText = document.querySelector('.loading-text')
 console.log(imgContainer)
 
 const url = 'https://source.unsplash.com/random';
 
 button.addEventListener('click', async () => {
+
     try {
+
         const resp = await Loadimage(url);
         imgContainer.appendChild(resp)
     } catch (error) {
@@ -16,8 +19,10 @@ button.addEventListener('click', async () => {
 const Loadimage = (url) => {
     return new Promise((resolve, reject) => {
         let img = new Image();
+        loadingText.style.display = 'block';
         img.addEventListener('load', () => {
             resolve(img)
+            loadingText.style.display = 'none';
         })
         img.addEventListener('error', () => {
             reject(new Error(`Opps there is error in your ${url}`))
