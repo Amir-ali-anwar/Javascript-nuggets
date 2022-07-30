17910fae6c8e257bdff5f6bf9524a44540df7402
Amir Ali Anwar <amiralianwar611@gmail.com>
2022-07-30 16:50:18 +0500

    fetching followers

--- Array.from-Pagination/app.js
+++ Array.from-Pagination/app.js
@@ -1,4 +1,10 @@
-import fetchFollowers from './fetchFollowers.js'
-import displayFollowers from './displayFollowers.js'
-import paginate from './paginate.js'
-import displayButtons from './displayButtons.js'
+import fetchFollowers from "./fetchFollowers.js";
+import displayFollowers from "./displayFollowers.js";
+import paginate from "./paginate.js";
+import displayButtons from "./displayButtons.js";
+
+const init = async() => {
+  const followers = await fetchFollowers();
+  console.log(followers);
+};
+window.addEventListener("load", init);
--- Array.from-Pagination/fetchFollowers.js
+++ Array.from-Pagination/fetchFollowers.js
@@ -1,5 +1,9 @@
-const url = 'https://api.github.com/users/john-smilga/followers?per_page=100'
+const url = "https://api.github.com/users/bradtraversy/followers?per_page=1000";
 
-const fetchFollowers = async () => {}
+const fetchFollowers = async () => {
+    const response= await fetch(url);
+    const data= await response.json()
+    return data
+}
 
 export default fetchFollowers
