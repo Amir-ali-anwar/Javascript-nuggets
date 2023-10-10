2172be4a5148cb3ac23a84fd48d06251d16f31f6
Amir Ali Anwar <amiralianwar611@gmail.com>
2023-10-10 21:28:45 +0500

    added the example

--- Form-Enteries/app.js
+++ Form-Enteries/app.js
@@ -1,11 +1,33 @@
 let form = document.querySelector('.form');
 form?.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const formData = [... new FormData(e.currentTarget).values()];
-   if(formData.includes('')){
-     throw new Error("Please enter all the values")
-   }
-   const formObject= Object.fromEntries(formData)
-   console.log(formObject);
-   currentTarget.reset();
+  e.preventDefault();
+  const formData = [... new FormData(e.currentTarget).values()];
+  console.log(formData);
+  if (formData.includes('')) {
+    showError("Please enter all the values")
+    throw new Error("Please enter all the values")
+  }
+  const formObject = Object.fromEntries(formData)
+  let values = []
+  for (const [key, value] of formObject) {
+    values.push(`${key}: ${value}`);
+  }
+  const message = values.join('\n');
+  showSuccess(message)
+  currentTarget.reset();
 });
+function showError(msg) {
+  Swal.fire({
+    icon: 'error',
+    title: 'Oops...',
+    text: msg,
+  })
+}
+
+function showSuccess(msg) {
+  Swal.fire({
+    icon: 'Success',
+    title: 'Your form Values',
+    text: msg,
+  })
+}
