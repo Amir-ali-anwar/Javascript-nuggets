e9e253fa259a42a513f22bd068d941e49dcb48a8
Amir Ali Anwar <amiralianwar611@gmail.com>
2023-10-23 20:36:31 +0500

    removed duplicates

--- Unique-Values/app.js
+++ Unique-Values/app.js
@@ -6,3 +6,25 @@ let output = [... new Set(data)]
 let objectToArray = Array.from(output)
 console.log(Array.isArray(objectToArray))
 console.log(typeof objectToArray)
+
+
+// remove duplicates using the custom function
+
+const removeDuplicates=()=>{
+    console.log(arr);
+    let uniqueArr=[]
+    for (let index = 0; index < arr.length; index++) {
+        const element = arr[index];
+       if(!uniqueArr.includes(element)){
+        uniqueArr.push(element)
+       }
+        
+    }
+    return uniqueArr
+}
+
+let arr=[1, 2, 3, 2, 4, 1, 5]
+
+
+
+console.log(removeDuplicates(arr));
