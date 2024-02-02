a22217dc046f1e4327295ee9b07af30db1946623
Amir Ali anwar <amiralianwar611@gmail.com>
2024-02-02 21:32:11 +0500

    working on the Javascript crud

--- Javascript-Crud/Js/app.js
+++ Javascript-Crud/Js/app.js
@@ -1,31 +1,44 @@
-const form = document.getElementById('myForm');
-const dataTable = document.getElementById('dataTable');
+
+const [form, dataTable, singleItem] = [document.querySelector('#myForm'), document.querySelector('#dataTable'), document.querySelector('.items')];
+const deleteItem = document.querySelector('.remove-btn')
+console.log(deleteItem);
 const SetItemsInLocalStorage = (key, data) => {
     return localStorage.setItem(key, JSON.stringify(data))
 }
 const GetItemsFromLocalStorage = (key) => {
     return JSON.parse(localStorage.getItem(key)) || []
 }
+const generateUniqueId = () => {
+    const randomBytes = new Uint8Array(5)
+    window.crypto.getRandomValues(randomBytes)
+    const uniqueId = Array.from(randomBytes).map(byte => byte.toString(16).padStart(2, '0')).join('');
+    const timestamp = Date.now().toString(16).padStart(12, '0');
+
+    return uniqueId + timestamp;
+}
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(e.target)
     const name = formData.get('name')
     const email = formData.get('email')
-    const userData = { name, email }
+    const uniqueId = generateUniqueId()
+    const userData = { name, email, id: uniqueId }
     let localStorageData = GetItemsFromLocalStorage('Crud')
     const localStorageUpdatedData = localStorageData.concat(userData);
     SetItemsInLocalStorage('Crud', localStorageUpdatedData);
-    updateTable();
     form.reset()
+    updateTable();
 })
+
 const updateTable = () => {
     const localStorageData = GetItemsFromLocalStorage('Crud');
     console.log(localStorageData);
-    const singleItem = document.querySelector('.items'); 
-   return  singleItem.innerHTML = localStorageData?.map((item) => {
-        return `<div class="single-item">
-            <p>${item.email}</p>
+
+    return singleItem.innerHTML = localStorageData?.map((item) => {
+        console.log(item);
+        return `<div class="single-item" data-id=${item.id}>
             <p>${item.name}</p>
+            <p>${item.email}</p>
             <button class="btn remove-btn">delete</button>
         </div>`;
     }).join(" ");
