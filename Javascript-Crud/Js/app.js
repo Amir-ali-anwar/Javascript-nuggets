204245efd5beae7f2d756c586ee071c4d5dc3bf6
Amir Ali anwar <amiralianwar611@gmail.com>
2024-02-02 23:18:57 +0500

    working on the crud

--- Javascript-Crud/Js/app.js
+++ Javascript-Crud/Js/app.js
@@ -1,26 +1,26 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     const [form, singleItem,] = [document.querySelector('#myForm'), document.querySelector('.items')];
-    
-     singleItem.addEventListener('click',(event)=>{
-        if(event.target.classList.contains('remove-btn')){
+
+    singleItem.addEventListener('click', (event) => {
+        if (event.target.classList.contains('remove-btn')) {
             const itemId = event.target.closest('.single-item').dataset.id;
             deleteItem(itemId)
         }
-     })
+    })
     const SetItemsInLocalStorage = (key, data) => {
         return localStorage.setItem(key, JSON.stringify(data));
     };
     const GetItemsFromLocalStorage = (key) => {
         return JSON.parse(localStorage.getItem(key)) || [];
     };
-    
-    const deleteItem=(itemId)=>{
-        const storedItems= GetItemsFromLocalStorage('Crud')
-        const filteredItems= storedItems.filter((storedItem)=>storedItem.id !== itemId)
+
+    const deleteItem = (itemId) => {
+        const storedItems = GetItemsFromLocalStorage('Crud')
+        const filteredItems = storedItems.filter((storedItem) => storedItem.id !== itemId)
         SetItemsInLocalStorage('Crud', filteredItems);
         updateTable()
-        return 
+        return
     }
     const generateUniqueId = () => {
         const randomBytes = new Uint8Array(5);
@@ -45,15 +45,16 @@ document.addEventListener('DOMContentLoaded', () => {
         updateTable();
     });
     const localStorageDataLength = GetItemsFromLocalStorage('Crud');
-    if(localStorageDataLength.length ===0){
-        const Nodata= document.createElement('p')
-        Nodata.textContent="No Items in the list";
-        singleItem.innerHTML=Nodata
-        // document.body.appendChild(Nodata);
-        }
+    if (localStorageDataLength.length === 0) {
+        const Nodata = document.createElement('p')
+        Nodata.setAttribute('class', 'No-data');
+        const headingText = document.createTextNode("No Items in the list");
+        Nodata.appendChild(headingText)
+        document.querySelector('.section-center').appendChild(Nodata);
+    }
     const updateTable = () => {
         const localStorageData = GetItemsFromLocalStorage('Crud');
-       
+
         return singleItem.innerHTML = localStorageData?.map((item) => {
             return `<div class="single-item" data-id=${item.id}>
                 <p>${item.name}</p>
