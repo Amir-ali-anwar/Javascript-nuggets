75822e5d31312c71674cb0cc5e8377d218c599a9
Amir Ali anwar <amiralianwar611@gmail.com>
2024-02-17 22:22:05 +0500

    css variables

--- Css-variables/app.js
+++ Css-variables/app.js
@@ -1,8 +1,10 @@
 const toggleBtn= document.querySelector('.btn')
 const root=document.querySelector(':root')
-
+const heading3= document.querySelector('.post h3')
 toggleBtn.addEventListener('click',()=>{
     const rootStyles = getComputedStyle(root);
+    const headingStyles= getComputedStyle(heading3).getPropertyValue('letter-spacing')
+    console.log(headingStyles);
     const current = rootStyles.getPropertyValue('--clr-bcg');
     const colorValue = current === '#fff' ? '#282c35' : '#fff';
     const fontValue = current === '#fff' ? '#fff' : '#282c35';
@@ -11,3 +13,6 @@ toggleBtn.addEventListener('click',()=>{
 
 
 })
+const variableHeading= document.querySelector('.post h3')
+const headingStyles= getComputedStyle(variableHeading).getPropertyValue('margin')
+console.log(headingStyles);
