cc4b9140625a6553494abf86bcab484c09778145
Amir Ali Anwar <amiralianwar611@gmail.com>
2024-07-02 12:22:54 +0500

    added the additional image check

--- Image-Preview/app.js
+++ Image-Preview/app.js
@@ -1,37 +1,39 @@
-const formSubmit= document.querySelector('.submit--form')
-console.log(formSubmit);
-const image= document.querySelector('#fileInput')
-const imagePreview=document.querySelector('#image-preview')
+const formSubmit = document.querySelector('.submit--form');
+const image = document.querySelector('#fileInput');
+const imagePreview = document.querySelector('#image-preview');
+const Noimg = document.querySelector('#image-preview p');
 
-console.log(imagePreview.firstChild);
-const Noimg=document.querySelector('#image-preview p')
-formSubmit.addEventListener('submit',(event)=>{
-    console.log("click");
+formSubmit.addEventListener('submit', (event) => {
     event.preventDefault();
+    
     const previousImage = imagePreview.querySelector('img');
     if (previousImage) {
         imagePreview.removeChild(previousImage);
         Noimg.classList.remove('no-img');
     }
 
-    if (fileInput.files.length > 0) {
-        const file = fileInput.files[0]; // Get the first file
-        let fileReader= new FileReader()
-        console.log({fileReader});
+    if (image.files.length > 0) {
+        const file = image.files[0]; // Get the first file
+        const maxFileSizeInKB = 500;
+
+        // Check if the file size exceeds the limit
+        if (file.size > maxFileSizeInKB * 1024) {
+            alert("File size exceeds " + maxFileSizeInKB + "KB. Please choose a smaller file.");
+            return;
+        }
+
+        let fileReader = new FileReader();
         const imgElement = new Image();
-        fileReader.onload=(e)=>{
+        fileReader.onload = (e) => {
             const imageSrc = e.target.result;
-            imgElement.src=imageSrc
+            imgElement.src = imageSrc;
             imgElement.style.maxWidth = '100%';
             imgElement.style.height = '400px';
-            imgElement.style.objectFit='contain'
-            imagePreview.appendChild(imgElement)
-            Noimg.classList.add('no-img')
-          
-        }
-        fileReader.readAsDataURL(file)
+            imgElement.style.objectFit = 'contain';
+            imagePreview.appendChild(imgElement);
+            Noimg.classList.add('no-img');
+        };
+        fileReader.readAsDataURL(file);
     }
-    formSubmit.reset()
-})
-
-// console.log(image?.files[0]);
+    formSubmit.reset();
+});
