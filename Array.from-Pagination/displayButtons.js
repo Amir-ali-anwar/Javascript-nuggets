6875d8436a0864063e7d7040cef2168c76c69ba5
Amir Ali Anwar <amiralianwar611@gmail.com>
2024-09-25 21:11:45 +0500

    Bug fixing

--- Array.from-Pagination/displayButtons.js
+++ Array.from-Pagination/displayButtons.js
@@ -1,9 +1,12 @@
 const displayButtons = (container,pages,index) => {
-   let buttons= pages?.map((_,pageIndex)=>{
+  let buttons= pages?.map((_,pageIndex)=>{
+    console.log({pageIndex});
+    console.log({index});
     return `<button class="page-btn ${
-      pageIndex === index ? "active-btn" : "null"
+      pageIndex === index ? "active-btn" : null
     }" data-index=${pageIndex}>${pageIndex + 1}</button>`;
-   })
+  })
+  
    buttons.push('<button class="next-btn">next</button>')
    buttons.unshift('<button class="prev-btn">prev</button>');
    container.innerHTML=buttons.join('')
--- Array.from-Pagination/fetchFollowers.js
+++ Array.from-Pagination/fetchFollowers.js
@@ -2,7 +2,7 @@ const url = "https://api.github.com/users/bradtraversy/followers?per_page=1000";
 
 const fetchFollowers = async () => {
     const response = await fetch(url);
-    if (!response) throw new Error("Response not Valid")         
+    if (!response) throw new Error("Response is not Valid")         
     const data = await response.json()
     return data
 }
