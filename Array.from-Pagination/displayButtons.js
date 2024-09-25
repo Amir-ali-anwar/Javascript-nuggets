3564812409758a39ba484d883aebfe7b94ba2a74
Amir Ali Anwar <amiralianwar611@gmail.com>
2024-09-25 21:29:52 +0500

    Quick fix the issue

--- Array.from-Pagination/displayButtons.js
+++ Array.from-Pagination/displayButtons.js
@@ -1,9 +1,9 @@
 const displayButtons = (container,pages,index) => {
   let buttons= pages?.map((_,pageIndex)=>{
-    console.log({pageIndex});
-    console.log({index});
-    return `<button class="page-btn ${
-      pageIndex === index ? "active-btn" : null
+    console.log({ pageIndex, currentIndex: index, isActive: pageIndex === index });
+
+    return `<button data-value=${index} class="page-btn ${
+      pageIndex === index ? "active-btn" : ''
     }" data-index=${pageIndex}>${pageIndex + 1}</button>`;
   })
   
@@ -11,5 +11,8 @@ const displayButtons = (container,pages,index) => {
    buttons.unshift('<button class="prev-btn">prev</button>');
    container.innerHTML=buttons.join('')
 }
-
+const pageBtns = container.querySelectorAll('.page-btn');
+pageBtns.array.forEach(element => {
+  
+});
 export default displayButtons
